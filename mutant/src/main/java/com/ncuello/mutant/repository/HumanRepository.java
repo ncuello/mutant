a47abb0cdac14b865291464dfd785e5801c68a06
Nicolás <nicolascuello@outlook.com>
2020-10-14 23:37:42 -0300

    Se agrega repository

---
+++ mutant/src/main/java/com/ncuello/mutant/repository/HumanRepository.java
@@ -0,0 +1,10 @@
+package com.ncuello.mutant.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ncuello.mutant.model.Human;
+
+public interface HumanRepository extends JpaRepository<Human, Long> {
+
+	Human findByDna(String dna);
+}
