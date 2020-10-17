96719cdb4ce997ced096974cb2836eb9f5bfdd58
Nicolás <nicolascuello@outlook.com>
2020-10-16 21:02:43 -0300

    Agrego endpoint /stats

--- mutant/src/main/java/com/ncuello/mutant/controller/HumanController.java
+++ mutant/src/main/java/com/ncuello/mutant/controller/HumanController.java
@@ -1,17 +1,24 @@
 package com.ncuello.mutant.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ncuello.mutant.dto.HumanDTO;
+import com.ncuello.mutant.dto.StatsDTO;
 import com.ncuello.mutant.service.HumanService;
 
 @RestController
 public class HumanController {
+	
+	private static final Logger log = LoggerFactory.getLogger(HumanController.class);
 	
 	@Autowired
 	private HumanService service;
@@ -27,7 +34,17 @@ public class HumanController {
 				return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
 			}
 		} catch (Exception e) {
+			log.error(e.getMessage());
 			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
 		}
 	}
+	
+	@GetMapping("/stats")
+	@ResponseStatus(HttpStatus.ACCEPTED)
+	public StatsDTO stats() {
+		StatsDTO stats = service.getStats();
+		log.info(stats.toString());
+		return stats;
+	}
+	
 }
